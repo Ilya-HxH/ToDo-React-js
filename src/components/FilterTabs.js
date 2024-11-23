38aad1aada730db780156b7aa040a52e86ef4fda
Ilya <i-kim-05@mail.ru>
2024-11-23 11:52:58 +0500

    Step 3 add filter tabs

--- src/components/FilterTabs.js
+++ src/components/FilterTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function FilterTabs({ setFilter }) {
+    
     return (
         <div className="flex gap-4 mt-4">
             <button onClick={() => setFilter('all')} className="text-blue-600 hover:underline border-2 rounded px-1.5 border-blue-600 ">All list</button>
